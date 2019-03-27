85fd55b2d54c8920007df73d98e07d4576380a23
davyengone <davy@hackages.io>
2019-03-27 10:33:29 +0100

    starting point react-redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const Counter = props => {
+  return (
+    <Fragment>
+      <button onClick={props.increment}>Increment</button>
+      <button onClick={props.decrement}>Decrement</button>
+    </Fragment>
+  );
+};
+
+const Header = ({ counter }) => {
+  return <h1>Counter {counter}</h1>;
+};
+
+class App extends React.Component {
+  state = {
+    counter: 0
+  };
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  render() {
+    return (
+      <Fragment>
+        <Header counter={this.state.counter} />
+        <Counter increment={this.increment} decrement={this.decrement} />
+      </Fragment>
+    );
+  }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
